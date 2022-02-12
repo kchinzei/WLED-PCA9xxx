9cbea739965ef5ed7ba0addade9fdd2062d9687c
kchinzei <kchinzei@gmail.com>
2022-02-12 12:23:16 +0900

    Initial working files

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # WLED-PCA9xxx
-Private UserMod for WLED
+Private userMod for [WLED](https://github.com/Aircoookie/WLED.git)
 
-It's a usermod of WLED using my PCA9xxxPWM. 
-PCA9xxx is an I2C based PWM controller chip from NXP.
-I don't have a plan to ask a pull request, because it's too much my hardware dependent.
-Keeping github repository without issueing PR is not easy,
-because of mixture of edited files in the original WLED files.
+![usermod screen sample](usermod_fig.png "Usermod Setup Sample")
+
+It's a usermod of [WLED](https://github.com/Aircoookie/WLED.git) using [PCA9xxxPWM](https://github.com/kchinzei/PCA9xxxPWM.git) library.
+PCA9xxx is an I2C based PWM controller chips from NXP.
+
+At this moment I don't have a plan to ask a pull request, because it's [PCA9xxxPWM](https://github.com/kchinzei/PCA9xxxPWM.git) library specific  and not testable for many others.
+
+## Build it
+
+Basically, this repository keeps its own files apart from [WLED](https://github.com/Aircoookie/WLED.git) files.
+This makes following updates in [WLED](https://github.com/Aircoookie/WLED.git) easy.
+But you need to place the files into proper places before building [WLED](https://github.com/Aircoookie/WLED.git).
+
+- Copy `platformio_override.ini` to `WLED`.
+- Copy `usermods_list.cpp` to `WLED/wled00` (overwrite existing one).
+- Copy `extLibs` to `WLED/wled00`.
+
+In Linux or macOS you can also use `ln` terminal command so that any changes to these files inside `WLED` folder will be visible by git commands.
+
+## The UserMod
+
+This usermod add a functionality to control PWM value of PCA9xxx devices. It is tested on ESP-WROOM-2 (d_mini) and ESP32-WROOM-32 (esp32).
+
+### Current implementation
+
+- Hot plug in/out.
+- Device-wise activation on/off.
+- I2C address and device type (display only).
+- PWM value follows the main brightness slider.
+- All PWM channels use the same value.
+- I2C pins (SDA/SDL) fixed to the default of the hardware. [Pins 4 and 5 for esp8266](https://arduino-esp8266.readthedocs.io/en/latest/libraries.html#i2c-wire-library), [pins 21 and 22 for the generic ESP32 but can vary from board to board](https://docs.espressif.com/projects/arduino-esp32/en/latest/api/i2c.html).
+- Exponential PWM value (not uniform across PCA9955A and other devices.)
+
+### Future implementation
+
+- Channel-wise control of the PWM value.
+- Smooth change of PWM value.
+- Customizable I2C pins.
+- Exponential PWM value uniform across devices.
---
+++ extLibs/PCA9xxxControl/UsermodPCA9xxxControl.h
@@ -0,0 +1,177 @@
+#pragma once
+
+#include "wled.h"
+#include <PCA9xxxPWMFactory.h>
+#include <PCA9xxxPWM.h>
+#include <Wire.h>
+
+#define MAXDEVICE 4
+#define DEFAULT_PIN_OE 2
+
+class UsermodPCA9xxxControl : public Usermod {
+  private:
+    PCA9xxxPWMFactory factory;
+    PCA9xxxPWM *pwms[MAXDEVICE];
+    uint8_t ndevice = 0;
+    int8_t pinOE = DEFAULT_PIN_OE;
+    bool activate[MAXDEVICE];
+    bool exponential = false;
+
+    unsigned long lastTime = 0;
+
+  public:
+    /*
+     * setup() is called once at boot. WiFi is not yet connected at this point.
+     * You can use it to initialize variables, sensors or similar.
+     */
+    void setup() {
+      // Serial.begin(115200);
+      ndevice = factory.scanDevice(pwms, MAXDEVICE, true);
+
+      // Wire.setClock(400000);
+      Wire.setClock(100000);
+      // Setting OE pin handled by readFromConfig()
+      // pinMode(pinOE, OUTPUT);
+      // digitalWrite(pinOE, 0); // Low-active
+    }
+
+
+    /*
+     * connected() is called every time the WiFi is (re)connected
+     * Use it to initialize network interfaces
+     */
+    void connected() {
+      // Serial.println("Connected to WiFi!");
+    }
+
+
+    /*
+     * loop() is called continuously. Here you can check for events, read sensors, etc.
+     * 
+     * Tips:
+     * 1. You can use "if (WLED_CONNECTED)" to check for a successful network connection.
+     *    Additionally, "if (WLED_MQTT_CONNECTED)" is available to check for a connection to an MQTT broker.
+     * 
+     * 2. Try to avoid using the delay() function. NEVER use delays longer than 10 milliseconds.
+     *    Instead, use a timer check as shown here.
+     */
+
+    #define PCA9XXX_MAXVAL 1.0
+
+    void loop() {
+      float val = PCA9XXX_MAXVAL * bri / 255;
+      // float val = 1.0 * briS / 255;
+      if (val > 1.0) val = 1.0;
+      if (val < 0.0001) val = 0;
+
+      unsigned long timer = millis() - lastTime;
+      if (timer > 100) {
+        lastTime = millis();
+        ndevice = factory.scanDevice(pwms, MAXDEVICE);
+        for (int i = 0; i < ndevice; i++) {
+          PCA9xxxPWM *pwm = pwms[i];
+          if (!pwm->hasBegun()) {
+            initializePWM(pwm);
+          } else {
+            int n_of_ports = pwm->number_of_ports();
+            for (int j = 0; j < n_of_ports; j++) {
+              pwm->pwm(j, val * activate[i]);
+            }
+          }
+        }
+      }
+    }
+
+#define PCA9xxx_ROOT_KEY "PCA9xxx Status"
+#define PCA9xxx_OE_KEY "Output Enable"
+#define PCA9xxx_EXPONENTIAL "Exponential brightness"
+#define PCA9xxx_DEVICE_KEY "Device %d"
+#define PCA9xxx_ACTIVATE "Activate"
+#define PCA9xxx_ADDR "Addr"
+#define PCA9xxx_TYPE "Type"
+
+    void addToConfig(JsonObject& root)
+    {
+      JsonObject top = root.createNestedObject(PCA9xxx_ROOT_KEY);
+      JsonObject oe = top.createNestedObject(PCA9xxx_OE_KEY);
+      oe["pin"] = pinOE;
+      top[PCA9xxx_EXPONENTIAL] = exponential;
+
+      // For display purpose only
+      for (int i = 0; i < MAXDEVICE; i++) {
+        char buf[32];
+        sprintf(buf, PCA9xxx_DEVICE_KEY, i);
+        JsonObject device = top.createNestedObject(buf);
+        device[PCA9xxx_ACTIVATE] = activate[i];
+        if (i < ndevice) {
+          sprintf(buf, "0x%x", pwms[i]->get_i2cAddr());
+          device[PCA9xxx_ADDR] = buf;
+          device[PCA9xxx_TYPE] = pwms[i]->type_name();
+        } else {
+          device[PCA9xxx_ADDR] = "(n/c)";
+          device[PCA9xxx_TYPE] = "(n/c)";
+        }
+      }
+    }
+
+    bool readFromConfig(JsonObject& root)
+    {
+      // default settings values could be set here (or below using the 3-argument getJsonValue()) instead of in the class definition or constructor
+      // setting them inside readFromConfig() is slightly more robust, handling the rare but plausible use case of single value being missing after boot (e.g. if the cfg.json was manually edited and a value was removed)
+
+      JsonObject top = root[PCA9xxx_ROOT_KEY];
+
+      bool configComplete = !top.isNull();
+
+      JsonObject oe = top[PCA9xxx_OE_KEY];
+      configComplete &= !oe.isNull();
+      bool newExp = false;
+      configComplete &= getJsonValue(top[PCA9xxx_EXPONENTIAL], newExp, false);
+      setExponential(newExp);
+
+      int8_t pin = -1;
+      configComplete &= getJsonValue(oe["pin"], pin, DEFAULT_PIN_OE);
+      setOE(pin);
+
+      for (int i = 0; i < MAXDEVICE; i++) {
+        char buf[32];
+        sprintf(buf, PCA9xxx_DEVICE_KEY, i);
+        JsonObject device = top[buf];
+        configComplete &= !device.isNull();
+        configComplete &= getJsonValue(device[PCA9xxx_ACTIVATE], activate[i], 0);
+      }
+      return configComplete;
+    }
+
+  private:
+    void initializePWM(PCA9xxxPWM *pwm) {
+      if (pwm->isConnected()) {
+        if (pwm->begin() == false) {
+          // Serial.println("Fail. Attempt to reset");
+          pwm->reset();
+          pwm->begin();
+        }
+        pwm->exponential_adjustment(exponential);
+      }
+    }
+
+    void setExponential(bool newMode) {
+      if (newMode != exponential) {
+        for (int i = 0; i < ndevice; i++) {
+          pwms[i]->exponential_adjustment(newMode);
+        }
+        exponential = newMode;
+      }
+    }
+
+    void setOE(int8_t newPinOE) {
+      if (pinOE != newPinOE) {
+        pinMode(pinOE, INPUT_PULLUP);
+        pinOE = newPinOE;
+      }
+      if (pinOE >= 0) {
+        pinMode(pinOE, OUTPUT);
+        digitalWrite(pinOE, 0); // Low-active
+      }
+    }
+};
